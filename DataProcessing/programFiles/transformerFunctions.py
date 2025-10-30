c4f841bdbb9d8e2235e5522b57f3eecc7da78bce
eveuio <evelyn.jones1613@gmail.com>
2025-10-30 17:04:33 -0500

    Ignore all __pycache__ folders

--- DataProcessing/programFiles/transformerFunctions.py
+++ DataProcessing/programFiles/transformerFunctions.py
@@ -114,7 +114,17 @@ class Transformer:
         b = math.log(2)/(1/(self.hotSpotWindingTemp_rated +273)- 1/(self.hotSpotWindingTemp_rated +273+6))
         a = math.e**(math.log(180000)-b/(self.hotSpotWindingTemp_rated+273))
 
-        #TODO: Collect max winding temp data from 
+        #TODO: Collect max winding temp data from {transformer.name}_average_metrics_hour
+        table_name = self.name + f'''{self.name}_average_metrics_hour'''
+        query = f'''SELECT * FROM "{table_name}" ORDER BY "DATETIME" ASC'''
+        transformerData = pandas.read_sql_query(query, self.conn)
+        transformerData['DATETIME'] = pandas.to_datetime(transformerData['DATETIME'])
+
+        # Identify relevant columns: Hot spot data
+        hsA, hsB, hsC = transformerData.columns[1:4]
+
+        # Calculate max hotspot
+        transformerData['hotspot_temp_max'] = numpy.max(transformerData[[hsA, hsB, hsC]].values, axis=1)
 
         #TODO: Compute lifetime given data from phaseMax at given timestamp (ie, phase with largest recorded winding temp)
         
