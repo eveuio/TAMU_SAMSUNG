272a4f64c4e1e6597719be0f2b4a03553ad01d93
eveuio <evelyn.jones1613@gmail.com>
2025-10-30 14:57:59 -0500

    adding package functionality to Database and Transformer

---
+++ DataProcessing/__init__.py
@@ -0,0 +1,9 @@
+"""
+DataProcessing package
+
+This package contains all modules and subpackages related to
+data handling, database management, and transformer operations.
+"""
+
+# Expose subpackages for convenience
+from . import programFiles
