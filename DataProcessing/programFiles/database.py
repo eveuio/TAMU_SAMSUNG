1a318c9b269cf6c5101dffa753df5bdace9b9f44
eveuio <evelyn.jones1613@gmail.com>
2025-11-16 23:35:13 -0600

    adding removal of healthscores to delete method in database

--- DataProcessing/programFiles/database.py
+++ DataProcessing/programFiles/database.py
@@ -179,6 +179,8 @@ class Database:
         # Step 5: populate transient lifetime consumption
         self.write_lifetime_transient_df(transformer_name)
 
+        #Step 6: run forecasting method
+
         return
         
     #! Remove transformer rated values from master table and all associated metrics tables   
@@ -187,7 +189,7 @@ class Database:
             if not xfmr_name:
                 raise HTTPException(status_code=404, detail="Transformer not found")
 
-            # Drop related tables
+            # related tables to drop 
             tables_to_drop = [
                 f"{xfmr_name}_average_metrics_hour",
                 f"{xfmr_name}_average_metrics_day",
@@ -199,14 +201,20 @@ class Database:
                 f"{xfmr_name}_validationData",
                 ]
             
-            # Step 2: Drop related tables safely
             with self.engine.begin() as conn:
+                # Step 2: Drop related tables safely
                 for table in tables_to_drop:
                     conn.execute(text(f'DROP TABLE IF EXISTS "{table}"'))
 
                 # Step 3: Remove forecast data
                 conn.execute(
                     text("DELETE FROM ForecastData WHERE transformer_name = :name"),
+                    {"name": xfmr_name}
+                )
+    
+                #Step 4: remove healthscore data:
+                conn.execute(
+                    text("DELETE FROM HealthScores WHERE transformer_name = :name"),
                     {"name": xfmr_name}
                 )
 
